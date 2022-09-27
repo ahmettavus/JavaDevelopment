6efee5f3c5c1f21108ae0ca8d2465614618b866f
ahmettavus <ahmettavus40@gmail.com>
2022-09-27 22:22:46 +0200

    Java ATM Project

---
+++ HelloWorld/src/Patika/TemelKavramVeDegiskenler/AtmProjesi.java
@@ -0,0 +1,18 @@
+package Patika.TemelKavramVeDegiskenler;
+
+import java.util.Scanner;
+
+public class AtmProjesi {
+    public static void main(String[] args) {
+        String userName, password;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lutfen Kullanici Adinizi Giriniz : ");
+        userName = input.nextLine();
+        System.out.print("Lutfen Parolanizi Giriniz : ");
+        password = input.nextLine();
+
+
+
+    }
+}
